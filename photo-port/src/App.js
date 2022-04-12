2e421d13f02e94a8de19d4654648f33b70369c62
ChotanSharma <chotansharma@yahoo.com>
2022-04-11 20:47:06 -0400

    add the change

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -4,12 +4,14 @@ import Nav from './components/Nav';
 
 import About from './components/About';
 
+import Gallery from "./components/Gallery";
+
 function App() {
   return (
     <div >
-      <Nav>
-      </Nav>
+      <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
