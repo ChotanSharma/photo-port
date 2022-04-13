2e84389d9454ef89ce4bf83aacdc6e1a175e290b
ChotanSharma <chotansharma@yahoo.com>
2022-04-13 14:03:11 -0400

    add the form

---
+++ photo-port/src/components/contact/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function ContactForm() {
+    return (
+        <section>
+          <h1>Contact me</h1>
+          <form id="contact-form">
+            //name input
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" name="name" />
+            </div>
+            // email input
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input type="email" name="email" />
+            </div>
+
+            // message text area
+
+            <div>
+                <label htmlFor="meassage">Message:</label>
+                <textarea name="message" rows="5"/>
+            </div>
+
+            <button type="submit">Submit</button>
+          </form>
+        </section>
+      )
+
+
+}
+
+
+export default ContactForm
